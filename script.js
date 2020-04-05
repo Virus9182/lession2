d62f159df412852f799a26e25fac9033a2011041
Virus9182 <63170743+Virus9182@users.noreply.github.com>
2020-04-05 14:25:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let money=20000;
+let income=10000;
+let addExpenses='Интернет = 500, Такси = 500, Коммуналка = 2000';
+let deposit=false;
+let mission=30000;
+let period=4;
+
+console.log(money);
+console.log(income);
+console.log(deposit);
+console.log(addExpenses.length);
+console.log('Период равен '+period+' месяцев');
+console.log('Цель заработать '+mission+' рублей');
+let aaddExpenses=addExpenses.toLowerCase();
+let budgetDay=money/30;
+console.log(budgetDay);
+
